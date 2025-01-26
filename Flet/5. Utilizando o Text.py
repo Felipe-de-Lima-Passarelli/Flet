de1ef8fba00cb9909d12fb299bde3761bef63704
Felipe <malamdorox@gmail.com>
2025-01-26 18:07:29 -0300

    Att

--- Flet/5. Utilizando o Text.py
+++ Flet/5. Utilizando o Text.py
@@ -1,6 +1,19 @@
 import flet as ft
 
 def main(page: ft.Page):
-    pass
+    page.fonts = {
+        "Dragon Slayer": "Fontes/Dragon Slayer.ttf"
+    }
 
-ft.app(target = main) #Inicializar o App com a função Main
+    t1 = ft.Text(
+        value = "Olá, mundo! Seja bem vindo ao curso de Flet do Programador Aventureiro",
+        style = ft.TextThemeStyle.DISPLAY_SMALL,
+        bgcolor = ft.colors.BLACK38,
+        color = ft.colors.WHITE38,
+        font_family = "Dragon Slayer"
+    )
+
+    page.add(t1)
+    print(page.fonts)
+
+ft.app(target = main, assets_dir = "Arquivos Úteis") #Inicializar o App com a função Main
